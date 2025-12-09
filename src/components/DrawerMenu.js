aaf295fcb85bd15b5617513785d4d41267c7aa54
ngoctuk36 <bngoctu629@gmail.com>
2025-12-09 19:30:01 +0700

    update 2

--- src/components/DrawerMenu.js
+++ src/components/DrawerMenu.js
@@ -21,7 +21,8 @@ import {
   FaWater,
   FaTimes,
   FaSignOutAlt,
-  FaRobot
+  FaRobot,
+  FaGift
 } from 'react-icons/fa';
 
 export default function DrawerMenu({ isOpen, onClose }) {
@@ -60,6 +61,7 @@ export default function DrawerMenu({ isOpen, onClose }) {
     { path: '/tiet-kiem', label: 'Tiết kiệm tài nguyên', icon: <FaWater size={20} /> },
     { path: '/y-tuong', label: 'Ý tưởng xanh', icon: <FaLightbulb size={20} /> },
     { path: '/chat-bot', label: 'Chatbot AI', icon: <FaRobot size={20} />},
+    { path: '/doi-qua', label: 'Đổi quà', icon: <FaGift size={20} />},
     { divider: true },
     { path: '/cai-dat', label: 'Cài đặt', icon: <FaCog size={20} /> },
   ];
